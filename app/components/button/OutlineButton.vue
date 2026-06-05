e39b0d6a3796e1bd3f3993f34c28f8f5d0d37055
Amin Hadei <aminhadei@gmail.com>
2026-06-05 20:31:52 +0330

    feat(app): add layered and outline button

---
+++ app/components/button/OutlineButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  label?: string
+  href?: string
+}>(), {
+  label: '',
+})
+</script>
+
+<template>
+  <component
+    :is="href ? 'a' : 'button'"
+    :href="href"
+    :type="href ? undefined : 'button'"
+    class="flex h-14 cursor-pointer items-center rounded border-2 border-foreground bg-transparent px-6 py-2 font-lalezar text-lg! font-bold tracking-widest text-foreground no-underline shadow-none transition duration-200 outline-hidden hover:-translate-y-1 hover:shadow-md active:translate-x-0 active:translate-y-0 active:shadow-none lg:px-8 lg:py-3 lg:text-lg"
+  >
+    {{ label }}
+  </component>
+</template>
